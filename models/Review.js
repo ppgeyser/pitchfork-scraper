01ef3cef9f7461a77917ef93f100b76213a162c4
Peter Geyser <peterpgeyser@gmail.com>
2019-10-24 10:50:56 -0700

    Models finalized

---
+++ models/Review.js
@@ -0,0 +1,48 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the mongoose Schema constructor
+var Schema = mongoose.Schema;
+
+var ReviewSchema = new Schema({
+    // artist name is required and of type String
+    artist: {
+      type: String,
+      required: true
+    },
+    // album name is requred and of type String
+    album: {
+        type: String,
+        required: true
+    },
+    // img (for album art) is requred and of type String
+    img: {
+        type: String,
+        required: true
+    },
+    // link to review is required and of type String
+    link: {
+      type: String,
+      required: true
+    },
+    // This allows us to populate the Review with an associated Note
+    note: {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    },
+    // This ensures that any review that is saved doesn't not get dropped from the database
+    saved: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    // This ensures that we do not scrape and save duplicates
+    unique: true
+    
+  });
+  
+  // This creates our model from the above schema, using mongoose's model method
+  var Review = mongoose.model("Review", ReviewSchema);
+  
+  // Export the Review model
+  module.exports = Review;
+  
