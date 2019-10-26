ec3d2a1913627e41b7503f2d1dce79ff5282e4d6
Peter Geyser <peterpgeyser@gmail.com>
2019-10-26 09:10:03 -0700

    Added pseudocode

---
+++ public/app.js
@@ -0,0 +1,25 @@
+function printComments () {
+    // Grab comments based on custom data attribute set for each review card
+}
+
+// Scrape function
+
+    // On click for scrape button
+        
+        // AJAX get request to /scrape
+
+            //Query database for new scraped reviews 
+
+                //Clear all review cards
+
+                    //Reprint all review cards under card div with ID "reviews"
+
+// Add comments function
+
+    // Comment button on click handler
+
+        //AJAX post request to add new comment to Comment collection
+
+            //ID where comment to be posted grabed from custom data attribute that holds review ID
+
+                // Clear comments div and then reprint all new comments
