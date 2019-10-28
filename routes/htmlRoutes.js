729aee1a49fd4dc23c5c0045442c7f6a593068c2
Peter Geyser <peterpgeyser@gmail.com>
2019-10-27 20:51:31 -0700

    Set up second html route for individual review view

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,19 @@ module.exports = function(app) {
     });
   });
 
+  // Load individual review with comments page
+  app.get("/reviews/:id", function(req, res) {
+    db.Review.findOne({
+      _id: req.params.id
+  })
+  .populate("comment")
+    .then(function(dbReview) {
+      res.render("comments", {
+        review: dbReview,
+      });
+    });
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
